6327b9b88651adf027f0c55fb0d8a5b9564f4461
Edwin Saul <edwinsaulpm@gmail.com>
2022-07-24 09:54:18 -0500

    changes on push

--- push.py
+++ push.py
@@ -1,23 +1,31 @@
 # file  push - ESaul1135
+
 from os import getcwd
 from os.path import isfile,isdir
+from platform import system  as platsys
+
 slash="/"
-
-
+if platsys()=="Windows":slash="\\"
 
 class filepush_data:
 
-    def __init__(self,ubication_path):
+    def __init__(self,location_path):
         self.data=[]
         try:
             file=open(location_path+slash+".filepush","r")
-            self.data=file.read()
-            print(type(self.data))
+            self.data=file.read().split("\n")
             file.close()
         except:
             print("error reading .filepush")
             print("error on path: "+location_path)
-            exit()
+        self.arguments_data=[]
+        self.sorting_data()
+
+    def sorting_data(self):
+
+        
+        
+        pass
 
 
 class arguments_push:
@@ -28,9 +36,7 @@ class arguments_push:
 
 def get_arguments_filepush():
     arguments=[]
-    file=open(".filepush","r")
-    print(file.read())
-    input(":::x")
+    a=arguments_push(getcwd())
     pass
 
 
@@ -52,7 +58,8 @@ if __name__=="__main__":
         f.write("# filepush -  by Edwin Saul - @ESaul1135\n")
         f.write("# '# '  for comment\n")
         f.write("# '## ' for set  destination path\n")
-        f.write("# write bellow extensions, file names or folder names to push")
+        f.write("# write bellow extensions, file names")
+        f.write("# or folder names to push")
         f.close()
 
     if isfile(".filepush"): push(str(getcwd()))
