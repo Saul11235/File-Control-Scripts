74780d6ce793424edbb68a46d123473ac3893078
Edwin Saul <edwinsaulpm@gmail.com>
2022-07-24 17:35:14 -0500

    push modified

--- push.py
+++ push.py
@@ -1,11 +1,65 @@
-# file  push - ESaul1135
+#
+#  file  push - ESaul1135
+#
+#  minimum destination address length
 
-from os import getcwd
+minimun_address_length=6  
+
+#--------------------
+
+from os import getcwd,listdir
 from os.path import isfile,isdir
 from platform import system  as platsys
 
+#--------------------
+
 slash="/"
 if platsys()=="Windows":slash="\\"
+
+#--------------------
+
+def concat_strings(*args):
+    string=""; first=True
+    for x in args:
+        if  first: first=False;string=str(x)
+        else: string+=" "+str(x)
+    return string    
+
+#--------------------
+class object_arguments:
+
+    def __init__(self,path):
+        self.path=path
+        self.arguments=[]
+        #content  analyzed
+        #path filenames
+        self.__dirs=[]
+        self.__files=[]
+        #arguments
+        self.__extensions=[]
+        self.__nameFiles=[]
+
+
+    def list(self): return self.arguments
+
+    def add_extension(self,extension):
+        if extension!=" "*len(extension):
+            self.arguments.append(extension)
+    
+    def __scan_path(self):
+        for x in listdir(self.path):
+            xx=self.path+slash+x
+            if isdir(xx):self.__dirs.append(xx)
+            elif isfile(xx):self.__files.append(xx)
+
+
+
+
+    def analyze_content(self):
+        self.__scan_path()
+        pass
+
+#--------------------
 
 class filepush_data:
 
@@ -18,32 +72,58 @@ class filepush_data:
         except:
             print("error reading .filepush")
             print("error on path: "+location_path)
-        self.arguments_data=[]
-        self.sorting_data()
+        a=self.sorting_arguments_filepush()
+        self.list_objects=self.generate_objects_arguments(a)
 
-    def sorting_data(self):
+    def list(self): return self.list_objects
 
-        
-        
-        pass
+    def sorting_arguments_filepush(self):
+        list_no_empty=[]
+        for x in self.data:
+            if len(x.split()):list_no_empty.append(x)
+        list_no_comment=[]    
+        for  x in list_no_empty:
+            if x.split()[0]!="#":list_no_comment.append(x)
+        list_arguments_to_push=[]    
+        #format list_arguments_to_push :  [ path_destination  [extensions]  ]      
+        list_aux_destination=[]
+        list_aux_extensions=[]
+        for x in list_no_comment:
+            split=x.split()
+            destination_path=concat_strings(*tuple(split[1:len(split)]))
+            extension=concat_strings(*tuple(split[0:len(split)]))
+            if split[0]=="##" and list_aux_destination==[]:
+                #first iteration
+                list_aux_destination.append(destination_path)
+            elif split[0]=="##" and list_aux_destination!=[]:
+                #new  path destination
+                list_aux_destination.append(list_aux_extensions)
+                list_arguments_to_push.append(list_aux_destination)
+                list_aux_extensions=[]
+                list_aux_destination=[destination_path]
+            else:list_aux_extensions.append(extension)
+        if len(list_aux_extensions):
+            list_aux_destination.append(list_aux_extensions)
+            list_arguments_to_push.append(list_aux_destination)
+        return list_arguments_to_push
 
-
-class arguments_push:
-
-    def __init__(self,destination_path):
-        a=filepush_data(getcwd())
-        self.destination_path=destination_path
-
-def get_arguments_filepush():
-    arguments=[]
-    a=arguments_push(getcwd())
-    pass
-
-
+    def generate_objects_arguments(self,list_arguments):
+        list_objects=[]
+        for x in list_arguments:
+            p=x[0] # destination path
+            if len(p)>minimun_address_length and isdir(p) and len(x[1]):
+                obj=object_arguments(x[0])
+                for extension in x[1]:obj.add_extension(extension)
+                obj.analyze_content()
+                list_objects.append(obj)
+        return list_objects
+                
+#--------------------
 
 def push(path):
-
-    get_arguments_filepush()
+    file_data=filepush_data(path)
+    local=object_arguments(path)
+    
     print("hello  ")
     print(path)
 
@@ -58,7 +138,7 @@ if __name__=="__main__":
         f.write("# filepush -  by Edwin Saul - @ESaul1135\n")
         f.write("# '# '  for comment\n")
         f.write("# '## ' for set  destination path\n")
-        f.write("# write bellow extensions, file names")
+        f.write("# write bellow extensions, file names\n")
         f.write("# or folder names to push")
         f.close()
 
