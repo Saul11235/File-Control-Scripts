2f391eaee792043ed3af222acfce4a42930541b5
Edwin Saul <edwinsaulpm@gmail.com>
2023-01-10 21:28:43 -0500

    filter name ok

--- code/function_filter_name.py
+++ code/function_filter_name.py
@@ -7,13 +7,20 @@
 # ------------------------------
 
 def __replace_only_one(full_text,part):
+    #function to replace the parts
     # if to_replace=""
-    if __is_on_string(full_text,part):
-        text=full_text[:]
-        return text
+    if not(__is_on_string(full_text,part)) or part=="":
+        return full_text[:]
     else:
-        replaced="dd"
-        # comparing
+        original=full_text[:]
+        changed=original[:].replace(part,"*"*len(part))
+        # vars to analize if change new character
+        count=full_text.find(part) #number of chars before the part
+        count=count+len(part)    
+        replaced=""
+        for x in range(len(full_text)):
+            if x<count: replaced=replaced+"*"
+            else: replaced=replaced+original[x]
         return replaced
 
 
@@ -31,34 +38,35 @@ def filter_name(name,filterr):
     # case specific
     if name==filterr: return True
     # case no name
-    if name=="": return False
+    if name==" "*len(filterr): return False
     # case * full text
     if __is_on_string(filterr,"*"):
         # case only a *
         if filterr=="*": return True
         # sorting
         list_filter=filterr.split("*")
-        # print(list_filter)
+        if not(len(list_filter)): return False
         new_text=name[:]
-        print("text strings")
-        print(new_text)
         for x in list_filter:
             if __is_on_string(new_text,x):
                 new_text=__replace_only_one(new_text,x)[:]
             else: return False
-        print(new_text)
         # compare newtext
         if new_text==len(new_text)*"*": return True
-        else: return False
+        else: 
+            # verify +final * case
+            if len(list_filter):
+                last=list_filter[len(list_filter)-1]
+                if last=="":return True
+            return False
     else: return False
 
 # ----------------------------------
 
 if __name__=="__main__":
     print("test filter name\n")
-    names=["hola","file.txt","holo.pdf","raaa.pdfkk"]
-    filters=["*.pdf","hola"]
-#    names=[]
+    names=["hola","file.txt","holo.pdf","raaa.pdfkk","dato3julio.dox","poli.mp3","poli.pdf"]
+    filters=["*.pdf","hola","dato*julio.dox","poli.*"]
     #-------
     for name in names:
         for fil in  filters:
