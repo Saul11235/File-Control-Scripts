8331839c4d00a7874dc4bf807072067cd749e90b
Edwin Saul <edwinsaulpm@gmail.com>
2023-01-14 00:51:08 -0500

    class data explorer

--- code/class_data_explorer.py
+++ code/class_data_explorer.py
@@ -1,5 +1,8 @@
 # class explorer .filepush filepull file
 
+
+
+
 def uncoment_line(text):
     if not(text.find("##"))==-1: return text.replace("##"," ## ")[:]
     else: 
@@ -30,13 +33,84 @@ def clear_spaces(text):
     return response
 
 #----------------------------
+
+def get_path_atributes(line_text):
+    text=line_text.replace("##","")
+    print(text)
+
+#----------------------------
+
+def is_string_path(line_text):
+    if line_text.find("##")!=-1: return True
+    else: return False
+
+#----------------------------
+
 def clear_list(list_to_clear):
     list_response=[]
     for x in list_to_clear:
         line=uncoment_line(x)
         clean_line=clear_spaces(line)
-        if clean_line!="":list_response.append(clean_line)
-    return list_response
+        if clean_line!="" and clean_line!="-":
+            list_response.append(clean_line)
+    return(list_response)
+
+#----------------------------
+def is_negative_input(string):
+    if string[0]=="-": return True
+    else: return False
+#----------------------------
+
+def compute_group(list_args):
+    print("#################################")
+    first=True
+    path="pp"
+    commands=[]
+    positive=[]
+    negative=[]
+    for x in list_args:
+        if first:
+            first=False
+            #clear path and commands
+            
+        else:
+            neg=clear_spaces(x[1:len(x)])
+            if is_negative_input(x): negative.append(neg)
+            else: positive.append(x)
+    print(list_args)
+    print(positive)
+    print(negative)
+
+
+#----------------------------
+
+def compute_text(text_to_order):
+    #
+    # return lists
+    # [  [path, commands, positive_args, negative_args ] ..... ]
+    #
+    split_text=str(text_to_order).split("\n")
+    clean_content=clear_list(split_text)
+    have_path=False
+    all_content=[]
+    content=[]
+    for x in clean_content:
+        if is_string_path(x):
+            have_path=True
+            if len(content):
+                all_content.append(content[:])
+                content=[x]
+            else: content.append(x)
+        else:
+            if have_path:
+                content.append(x)
+    if len(content):all_content.append(content)
+    #---------
+    for x in all_content:
+        compute_group(x)
+
+
+
 
 #----------------------------
 
@@ -63,10 +137,8 @@ class special_file:
         self.file_True  =[]
         self.file_False =[]
         #----------------
-        list_text_input=self.text.split("\n")
-        lines_uncoment=clear_list(list_text_input)
+        l=compute_text(self.text)
         #----------------
-        print(lines_uncoment)
 
 
 
@@ -91,9 +163,14 @@ if __name__=="__main__":
  *.xls
    ##corintians           \t   .
  holo #comment
+ -l
  kasjdfkl
 ## "kddk  ddd" clear_origin
  dkdkd
+ - ajskldj
+ -
+ o
+ -*.swp
     """
     print(nn)
     print("------------")
